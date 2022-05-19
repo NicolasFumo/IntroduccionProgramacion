569b9c7e4e1d9ec1d70eb187818b73e3a274bbe8
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-05-19 18:35:41 -0300

    Create Contenedor.cs

---
+++ 17:00 a 18:30/POO/Ejercicio Cafetera/Version 2.1 (sin terminar)/Contenedor.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjericioCafetera
+{
+    public class Contenedor
+    {
+        public int Contenido;
+        public int Maximo;
+        public int Minimo;
+
+        public Contenedor(int maximo, int minimo)
+        {
+            Maximo = maximo;
+            Minimo = minimo;
+        }
+
+        public Contenedor()
+        {
+
+        }
+
+        public void MostrarContenido(string tipoLiquido)
+        {
+            Console.WriteLine($"El contenido de {tipoLiquido} es: {Contenido}");
+        }
+
+        public void Rellenar(string tipoLiquido)
+        {
+            Console.Write($"Ingrese cantidad de {tipoLiquido}: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            if (Contenido + cantidad > Maximo)
+            {
+                Contenido = Maximo;
+            }
+            else
+            {
+                Contenido += cantidad;
+            }
+        }
+    }
+}
