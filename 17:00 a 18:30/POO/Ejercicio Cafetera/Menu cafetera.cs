915ad6f484ebec108912daba74d572b7e5b82751
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-05-19 17:34:57 -0300

    Create Menu cafetera.cs

---
+++ 17:00 a 18:30/POO/Ejercicio Cafetera/Menu cafetera.cs
@@ -0,0 +1,36 @@
+int opcion = 0;
+
+Console.WriteLine("Para cafe fuerte ingrese 1");
+Console.WriteLine("Para cafe suave ingrese 2");
+Console.WriteLine("Para cafe con leche ingrese 3");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Para rellenar agua ingrese 4");
+Console.WriteLine("Para rellenar cafe ingrese 5");
+Console.WriteLine("Para rellenar leche ingrese 6");
+
+opcion = int.Parse(Console.ReadLine());
+
+if (opcion == 1)
+{
+
+}
+else if (opcion == 2)
+{
+
+}
+else if (opcion == 3)
+{
+
+}
+else if (opcion == 4)
+{
+
+}
+else if (opcion == 5)
+{
+
+}
+else if (opcion == 6)
+{
+
+}
