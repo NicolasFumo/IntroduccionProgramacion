f7bc32af7a3c1b0f57a7221e1b3c923e80c5c75d
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-06-01 16:37:23 -0300

    Create Program.cs

---
+++ 15:00 a 16:30/POO/Ejercicio Cafetera/Version 1/Program.cs
@@ -0,0 +1,11 @@
+
+using EjercicioCafetera2;
+
+Cafetera cafetera = new Cafetera();
+
+cafetera.Encender();
+cafetera.MostrarContenido();
+
+cafetera.PrepararSuave();
+cafetera.PrepararFuerte();
+cafetera.PrepararConLeche();
