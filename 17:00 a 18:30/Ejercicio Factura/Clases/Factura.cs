6ad91a9382739b3e56296f8d07daddd714465b67
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-06-14 17:56:22 -0300

    Create Factura.cs

---
+++ 17:00 a 18:30/Ejercicio Factura/Clases/Factura.cs
@@ -0,0 +1,17 @@
+public class Factura
+    {
+        public RazonSocial Emisor;
+        public RazonSocial Receptor;
+        public int NroFactura;
+        public char TipoFactura;
+        public List<ItemFactura> ItemsFactura;
+
+        public Factura()
+        {
+            Emisor = new RazonSocial();
+            Receptor = new RazonSocial();
+            ItemsFactura = new List<ItemFactura>();
+        }
+
+
+    }
