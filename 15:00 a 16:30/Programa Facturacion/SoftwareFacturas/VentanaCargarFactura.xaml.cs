04fab93f319b095914521333c5194abc0fdad178
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-06-29 15:09:42 -0300

    Add files via upload

---
+++ 15:00 a 16:30/Programa Facturacion/SoftwareFacturas/VentanaCargarFactura.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SoftwareFacturas.Clases;
+using System.Windows;
+
+namespace SoftwareFacturas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaCargarFactura.xaml
+    /// </summary>
+    public partial class VentanaCargarFactura : Window
+    {
+        public Factura FacturaACargar;
+        public VentanaCargarFactura()
+        {
+            InitializeComponent();
+
+            FacturaACargar = new Factura();
+
+            
+        }
+
+        private void btnCargarItemFactura_Click(object sender, RoutedEventArgs e)
+        {
+            ItemFactura item = new ItemFactura();
+
+            item.Descripcion = txtDescripcion.Text;
+            item.PrecioUnitario = decimal.Parse(txtPrecio.Text);
+            item.Cantidad = int.Parse(txtCantidad.Text);
+
+            FacturaACargar.ListadoItems.Add(item);
+
+            txtDescripcion.Text = "";
+            txtPrecio.Text = "";
+            txtCantidad.Text = "";
+
+            dtgItemsFactura.ItemsSource = null;
+            dtgItemsFactura.ItemsSource = FacturaACargar.ListadoItems;
+        }
+    }
+}
