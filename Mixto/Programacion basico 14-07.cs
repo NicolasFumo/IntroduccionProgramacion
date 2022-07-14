6ad091e5240392d5007902e51f0fb3bb137f5de9
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-07-14 18:25:56 -0300

    Create Programacion basico 14-07.cs

---
+++ Mixto/Programacion basico 14-07.cs
@@ -0,0 +1,19 @@
+// CLASE: INTRODUCCION FECHA: 14/07
+
+// crear o DECLARAR una variable
+string nombrePersona;
+string apellidoPersona;
+
+// guardar un dato en una variable
+nombrePersona = "Nicolas";
+apellidoPersona = "Fumo";
+
+// leo la variable y la muestro en pantalla
+//Console.WriteLine(nombrePersona);
+//Console.WriteLine(apellidoPersona);
+
+// concatenar string
+//Console.WriteLine("Bienvenido: " + nombrePersona + " " + apellidoPersona + "!");
+
+// interpolacion de strings
+Console.WriteLine($"Bienvenido: {nombrePersona} {apellidoPersona} !");
