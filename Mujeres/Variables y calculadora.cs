234ec551061d85bafc0111b3fe0d32072cfdead6
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-07-18 16:50:42 -0300

    Create Variables y calculadora.cs

---
+++ Mujeres/Variables y calculadora.cs
@@ -0,0 +1,32 @@
+// CODIGO CLASE 18/07/2022
+
+int primerNumero;
+int segundoNumero;
+int resultado;
+
+primerNumero = 9;
+segundoNumero = 3;
+
+resultado = primerNumero + segundoNumero;
+
+Console.WriteLine(resultado);
+
+
+// Declaracion de una variable
+// <tipo de dato> <Nombre>;
+int Edad; // variable para guardar numeros enteros (int)
+string Apellido; // para guardar textos (string)
+
+// <NombreVariable> = <DatoAGuardar>;
+Edad = 22;
+
+// Console.WriteLine(<NombreVariable>);
+Console.WriteLine(Edad);
+
+Edad = 70;
+
+Console.WriteLine(Edad);
+
+Apellido = "Fumo";
+
+Console.WriteLine(Apellido);
