75ea351fc069640e58b52ef07274c429a8a2483c
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2022-07-21 17:13:19 -0300

    Create if-else.cs

---
+++ Mixto/if-else.cs
@@ -0,0 +1,22 @@
+int edad;
+
+Console.Write("Ingrese su edad: ");
+edad = int.Parse(Console.ReadLine());
+
+// condicional
+// if( valor-comparar | operador | valor-comparado )
+if (edad >= 18 && edad > 50)
+{
+    if (edad < 50)
+    {
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+else 
+{
+    if (edad < 5)
+    {
+
+    }
+}
